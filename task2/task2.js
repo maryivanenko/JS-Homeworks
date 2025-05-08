5c20980810d2de5f9c61307e8654e4a056f9aeae
maryivanenko <mariaivanenko10b@gmail.com>
2025-05-08 11:05:27 +0300

    Task 2 done

---
+++ task2/task2.js
@@ -0,0 +1,9 @@
+/*Напиши выражение, которое вычисляет сумму двух чисел и умножает результат на третье число. 
+Выведи результат в консоль;*/
+
+let firstNumber = 7; // вводим числа//
+let secondNumber = 15;
+let thirdNumber = 2;
+let result = (firstNumber + secondNumber) * thirdNumber; // выражение для посчета суммы и умножения//
+console.log(`(${firstNumber} + ${secondNumber}) * ${thirdNumber}`); // выведет само выражение текстом //
+console.log(result); // выведет результат выражения//
